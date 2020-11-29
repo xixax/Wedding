b66b2f00e078afeb9963a1c5dd84ebecc504d5c0
Joao Sa <joaodiogogoncalvesa@gmail.com>
2020-11-29 11:53:37 +0000

    Trying to parse json file with GSON Getting exception. I was not able to use retrofit also

--- app/src/main/java/com/e/wedding/app/model/Guest.kt
+++ app/src/main/java/com/e/wedding/app/model/Guest.kt
@@ -1,3 +1,20 @@
 package com.e.wedding.app.model
 
-data class Guest(val username: String, val password: String)
+data class Guest(
+    val username: String,
+    val password: String,
+    //Image that will appear as pages. There will be a option to download
+    val conviteImagem: String,
+    val menuImagem: String,
+    val lembrancaImagem: String,
+
+    //Text strings
+    //Invite
+    val conviteAceite: String,
+    val conviteDetalhes: String,
+
+    //Breakfast
+    val pequenoAlmoco: String,
+    val pequenoAlmocoDetalhes: String,
+    val googleMapsPequenoAlmoco: String
+)
