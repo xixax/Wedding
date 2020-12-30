1b4a5213e73a0e25694a408ea57d28e7d175fb8e
Joao Sa <joaodiogogoncalvesa@gmail.com>
2020-12-30 14:09:07 +0000

    I have added the login button to the main activity

---
+++ app/src/main/java/com/e/wedding/app/view/main/login/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.e.wedding.app.view.main.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.e.wedding.R
+import com.e.wedding.app.model.DataHolder
+import com.e.wedding.app.view.main.home.HomeViewModel
+
+class LoginFragment : Fragment() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        textView.text = DataHolder.getGuest()?.boda
+
+
+        return root
+    }
+}
