017cbe28c9b9cde28389b7b91cd1f843640f4be7
xixax <joaodiogogoncalvesa@gmail.com>
2021-04-11 15:56:51 +0100

    Added engagement nav

--- app/src/main/java/com/e/wedding/app/view/main/MainActivity.kt
+++ app/src/main/java/com/e/wedding/app/view/main/MainActivity.kt
@@ -126,7 +126,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_invite, R.id.nav_ceremony
+                R.id.nav_home, R.id.nav_invite, R.id.nav_ceremony, R.id.nav_engagement
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
