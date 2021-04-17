6ee6651ac008c750e03275e9b1d7612f5cab762b
xixax <joaodiogogoncalvesa@gmail.com>
2021-04-17 18:55:21 +0100

    Menu download done

--- app/src/main/java/com/e/wedding/app/view/main/menu/MenuFragment.kt
+++ app/src/main/java/com/e/wedding/app/view/main/menu/MenuFragment.kt
@@ -1,6 +1,11 @@
 package com.e.wedding.app.view.main.menu
 
+import android.app.AlertDialog
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +14,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.e.wedding.R
 import com.e.wedding.app.model.DataHolder
-import com.e.wedding.app.view.main.engagement.EngagementViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.DataInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.net.InetAddress
+import java.net.URL
+
 
 class MenuFragment : Fragment() {
 
@@ -24,6 +36,77 @@ class MenuFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_menu, container, false)
         val textView: TextView = root.findViewById(R.id.text_menu)
         textView.text = DataHolder.getGuestLoggedIn()?.convitetext1 ?: "User ainda nao fez login"
+
+        val downloadMenu: FloatingActionButton = root.findViewById(R.id.menu_pdf_download)
+        downloadMenu.setOnClickListener {
+            val thread = Thread {
+                try {
+                    InetAddress.getByName("google.com")
+                    menuPdfDownload()
+                } catch (e: Exception) {
+                    activity?.runOnUiThread {
+                        showErrorNeutralMessage(
+                                resources.getString(R.string.internet_title_dialog_alert),
+                                resources.getString(
+                                        R.string.internet_message_dialog_alert
+                                ),
+                                resources.getString(R.string.okay)
+                        )
+                    }
+                }
+            }
+            thread.start()
+        }
+
         return root
+    }
+
+    private fun menuPdfDownload(){
+        try {
+            if(DataHolder.getGuestLoggedIn()!=null)
+            {
+                val downloadmanager = activity?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
+                val uri: Uri = Uri.parse(DataHolder.getGuestLoggedIn()?.menuDocumento)
+
+                val request = DownloadManager.Request(uri)
+                request.setTitle("JD_Wedding_Menu")
+                request.setDescription("Downloading")
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "JD_Wedding_Menu.pdf")
+                downloadmanager!!.enqueue(request)
+            }else{
+                activity?.runOnUiThread {
+                    showErrorNeutralMessage(
+                            resources.getString(R.string.download_menu_not_found_title),
+                            resources.getString(
+                                    R.string.download_menu_not_found_text
+                            ),
+                            resources.getString(R.string.okay)
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            activity?.runOnUiThread {
+                showErrorNeutralMessage(
+                        resources.getString(R.string.download_menu_not_found_title),
+                        resources.getString(
+                                R.string.download_menu_not_found_text
+                        ),
+                        resources.getString(R.string.okay)
+                )
+            }
+        }
+    }
+
+    private fun showErrorNeutralMessage(title: String, msg: String, button_text: String) {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle(title)
+        builder.setMessage(msg)
+        builder.setNeutralButton(button_text) { dialog, _ ->
+            dialog.cancel()
+            dialog.dismiss()
+        }
+        builder.show()
     }
 }
