a4957bb035f30d677e228728ea334e9d8ba7ba7c
xixax <joaodiogogoncalvesa@gmail.com>
2021-12-29 21:57:12 +0000

    Trial of app icon. Some maintenance

--- app/src/main/java/com/e/wedding/app/view/home/HomeFragment.kt
+++ app/src/main/java/com/e/wedding/app/view/home/HomeFragment.kt
@@ -1,8 +1,5 @@
 package com.e.wedding.app.view.home
 
-import android.app.AlertDialog
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import com.bumptech.glide.Glide
@@ -11,10 +8,6 @@ import com.e.wedding.app.base.BaseFragment
 import com.e.wedding.app.base.viewBinding
 import com.e.wedding.app.base.viewModel
 import com.e.wedding.databinding.FragmentHomeBinding
-import java.io.IOException
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 class HomeFragment : BaseFragment(R.layout.fragment_home) {
     private val binding by viewBinding(FragmentHomeBinding::bind)
@@ -27,7 +20,7 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
 
     private fun setup() {
         Glide.with(this)
-            .load("https://drive.google.com/u/0/uc?id=1WXc8HlPGVC4h6FovNqnIYgRtfIMAEOAs&export=download")
+            .load("https://drive.google.com/u/0/uc?id=1hpDekq5YsE4isI5XXE7RqpXvM6zCoN6R&export=download")
             .into(binding.homeBackground)
     }
 }
